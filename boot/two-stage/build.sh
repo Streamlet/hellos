439582ae1c6ec2c07bbc6b57c51c3200bd3e1aba
Streamlet <streamlet@outlook.com>
2026-05-04 18:58:30 +0800

    re-org

--- boot/two-stage/build.sh
+++ boot/two-stage/build.sh
@@ -2,4 +2,4 @@
 
 nasm -f bin stage1.asm -o stage1.bin
 nasm -f bin stage2.asm -o stage2.bin
-nasm -f bin -dKERNEL ../keyboard/boot.asm -o kernel.bin
+nasm -f bin -dKERNEL ../shell/boot.asm -o kernel.bin
--- boot/two-stage/layout.asm
+++ boot/two-stage/layout.asm
@@ -1,4 +1,6 @@
 ; memory layout:
+%ifndef __LAYOUT_ASM_INCLUDED__
+%define __LAYOUT_ASM_INCLUDED__
 
 STAGE1_ADDRESS          equ 0x7C00  ; standard bootloader load address
 STAGE2_ADDRESS          equ 0x7E00  ; stage2 bootloader load address, one sector (512 bytes) after stage1
@@ -6,3 +8,5 @@ STAGE2_SECTOR_COUNT     equ 7       ; stage2 bootloader size in sectors (7 secto
 
 KERNEL_ADDRESS_SEGMENT  equ 0x8000  ; kernel load segment
 KERNEL_ADDRESS_OFFSET   equ 0x0000  ; kernel load offset
+
+%endif  ; __LAYOUT_ASM_INCLUDED__
