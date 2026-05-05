894fce8bf70432a3d54b3028c663d686b7071677
Streamlet <streamlet@outlook.com>
2026-05-05 16:31:33 +0800

    add clang format

--- kernel/8086/hello/kernel.c
+++ kernel/8086/hello/kernel.c
@@ -1,12 +1,12 @@
 #include "bootstrap.h"
 
-void print_string(const char* s) {
-  for (const char* p = s; *p != '\0'; p++) {
+void print_string(const char *s) {
+  for (const char *p = s; *p != '\0'; p++) {
     print_char(*p);
   }
 }
 
 void kernel_main() {
-  char* message = "\r\nHello, Kernel!\r\n";
+  char *message = "\r\nHello, Kernel!\r\n";
   print_string(message);
 }
